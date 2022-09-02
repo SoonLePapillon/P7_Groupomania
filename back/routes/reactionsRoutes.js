ad23fcfc4797625291010556700a0496a002c5b6
Mehdi <birkyo@gmail.com>
2022-09-02 13:00:49 +0200

    remove alert and view news has conditions

--- back/routes/reactionsRoutes.js
+++ back/routes/reactionsRoutes.js
@@ -4,6 +4,6 @@ import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post("/:postId/reactions", auth, reactionController.react); // attention à bien mettre le postId ici et non pas dans app.js car le controller est exploité ici
+router.post("/:postId/reactions", auth, reactionController.react);
 
 export default router;
