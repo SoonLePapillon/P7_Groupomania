1bfd044f4f7dfd13ac8e1dcd17de3a2acbcd883d
Mehdi <birkyo@gmail.com>
2022-09-06 23:26:43 +0200

    remove unused line in router / index.js

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 import Login from '@/views/LoginView.vue';
 import Signup from '@/views/SignupView.vue';
 import Home from '@/views/HomeView.vue';
-import { nextTick } from 'vue';
 
 const routes = [
   {
